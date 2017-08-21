0dd096c661569976fc8b6f43e32b9d68c326ce50
Nick Main <nickmaster24@gmail.com>
2017-08-20 23:31:00 -0400

    Updated WorkDTO and server code that builds it

--- MortalCommon/src/in/nickma/mortal/dtos/WorkDTO.java
+++ MortalCommon/src/in/nickma/mortal/dtos/WorkDTO.java
@@ -5,20 +5,34 @@ import java.io.Serializable;
 public class WorkDTO implements Serializable {
 
     private final String gridData;
+    private final Integer sizeX;
+    private final Integer sizeY;
     private final Integer startX;
     private final Integer startY;
 
     public WorkDTO(
             final String gridData,
+            final Integer sizeX,
+            final Integer sizeY,
             final Integer startX,
             final Integer startY) {
         this.gridData = gridData;
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
         this.startX = startX;
         this.startY = startY;
     }
 
     public String getGridData() {
         return gridData;
+    }
+
+    public Integer getSizeX() {
+        return sizeX;
+    }
+
+    public Integer getSizeY() {
+        return sizeY;
     }
 
     public Integer getStartX() {
--- MortalServer/src/in/nickma/mortal/MortalServer.java
+++ MortalServer/src/in/nickma/mortal/MortalServer.java
@@ -79,14 +79,15 @@ public class MortalServer {
                 if (!line.startsWith("\t\t\t<param name=\"FlashVars\"")) continue;
                 line = line.split("\"")[3];
                 String[] ss = line.split("(=|&)");
-                Integer boardX = Integer.parseInt(ss[1]);
-                Integer boardY = Integer.parseInt(ss[3]);
+                Integer sizeX = Integer.parseInt(ss[1]);
+                Integer sizeY = Integer.parseInt(ss[3]);
                 String boardString = ss[5];
-                for (int x = 1; x <= boardX; x++) {
-                    for (int y = 1; y <= boardY; y++) {
-                        workStack.push(new WorkDTO(boardString, x, y));
+                for (int x = 1; x <= sizeX; x++) {
+                    for (int y = 1; y <= sizeY; y++) {
+                        workStack.push(new WorkDTO(boardString, sizeX, sizeY, x, y));
                     }
                 }
+                break;
             }
         } catch (IOException e) {
             closeProgramWithException(e);
