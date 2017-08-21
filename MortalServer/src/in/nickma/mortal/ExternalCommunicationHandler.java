bc9680d67141edd8662789b99d040eb66b597cdc
Nick Main <nickmaster24@gmail.com>
2017-08-20 21:44:30 -0400

    Added a server class to handle sending and recieving data from the hacker.org website

---
+++ MortalServer/src/in/nickma/mortal/ExternalCommunicationHandler.java
@@ -0,0 +1,39 @@
+package in.nickma.mortal;
+
+import java.io.InputStream;
+import java.net.URL;
+
+public class ExternalCommunicationHandler {
+
+    private final String username;
+    private final String password;
+
+    private static final String INITIAL_URL =
+            "http://www.hacker.org/coil/index.php?name=%s&password=%s";
+    private static final String SUBMIT_URL =
+            "http://www.hacker.org/coil/index.php?name=%s&password=%s&path=%s&x=%d&y=%d";
+
+    public ExternalCommunicationHandler(final String username, final String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public InputStream getInitialStream() {
+        return buildAndHandleURL(String.format(INITIAL_URL, username, password));
+    }
+
+    public InputStream getSubmissionsStream(final String path, final Integer x, final Integer y) {
+        return buildAndHandleURL(String.format(SUBMIT_URL, username, password, path, x, y));
+    }
+
+    private InputStream buildAndHandleURL(final String urlString) {
+        try {
+            URL url = new URL(urlString);
+            return url.openStream();
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+            return null;
+        }
+    }
+
+}
